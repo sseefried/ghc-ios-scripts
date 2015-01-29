f8319a0b16e53789661e495a643e9d5fec0b8a0e
Sean Seefried <sean.seefried@gmail.com>
2015-01-30 10:17:38 +1100

    Rename environment setup script

--- i386-env.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-env -i bash --rcfile "$HOME/ghc-ios/ghc-ios-scripts/i386-env.rc"
---
+++ ios-i386-env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ "$IOS_SCRIPTS" = "" ]; then
+  echo "IOS_SCRIPTS environment variable is not set"
+  exit 1
+fi
+
+env -i bash --rcfile "$IOS_SCRIPTS/ios-i386-env.rc"
